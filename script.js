3e20f17e2f7ee7d093afd9b62f78971584643123
Bilal <mansuribilal101@gmail.com>
2022-07-15 23:02:40 +0530

    Feat: UI updated

---
+++ script.js
@@ -0,0 +1,4 @@
+"use strict";
+const inputText = document.querySelector("#translate-input");
+const outputText = document.querySelector("#translate-output");
+const translateBtn = document.querySelector(".btn");
